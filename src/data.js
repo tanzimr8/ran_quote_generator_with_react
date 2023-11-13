851f0d8655d00c8aa60d0591cea124015ded20ad
Tanzim <tanzim008@gmail.com>
2023-11-13 17:25:27 -0330

    first commit

---
+++ src/data.js
@@ -0,0 +1,20 @@
+const quotes = [
+    {
+        id:1,
+        name: 'Casey Neistat',
+        title: 'Web Developer',
+        quote: " The measure of success is not how much time you spend doing what you love, it's how little time you spend doing what you hate."
+    },
+    {
+        id:2,
+        name: 'Mary Macdonald',
+        title: 'Web Developer',
+        quote: "If you want to build a ship, don't drum up the people to gather wood, divide the work, and give orders. Instead, teach them to yearn for the vast endless sea."
+    },
+    {
+        id:3,
+        name: 'Caroline Harper',
+        title: 'Web Developer',
+        quote: " We do not want merely to see beauty. We want something else which can hardly be put into words — to be united with the beauty we see, to pass into it, to receive it into ourselves."
+    }
+];
